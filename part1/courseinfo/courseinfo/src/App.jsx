e019356a64da18b8dd55cc2c3f785cdd1dfe6a07
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-02 12:01:50 +0900

    1.2 completed

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -27,15 +27,17 @@ const Header = ({course}) => {
 const Content = ({props}) => {
   return (
     <div>
-      <p>
-        {props.p1} {props.e1}
-      </p>
-      <p>
-        {props.p2} {props.e2}
-      </p>
-      <p>
-        {props.p3} {props.e3}
-      </p>
+      <Part props={{part: props.p1, exercises: props.e1}} />
+      <Part props={{part: props.p2, exercises: props.e2}} />
+      <Part props={{part: props.p3, exercises: props.e3}} />
+    </div>
+  )
+}
+
+const Part = ({props}) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
     </div>
   )
 }
