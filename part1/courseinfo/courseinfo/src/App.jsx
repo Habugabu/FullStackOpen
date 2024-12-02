c8c74a954c9efbd498cb8450672b53583fbe584d
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-02 12:17:39 +0900

    1.4 completed

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -1,23 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={{p1: part1, p2: part2, p3: part3}} />
-      <Total totalex={part1.exercises+part2.exercises+part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -33,9 +35,9 @@ const Header = ({course}) => {
 const Content = ({parts}) => {
   return (
     <div>
-      <Part part={parts.p1} />
-      <Part part={parts.p2} />
-      <Part part={parts.p3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   )
 }
@@ -48,7 +50,8 @@ const Part = ({part}) => {
   )
 }
 
-const Total = ({totalex}) => {
+const Total = ({parts}) => {
+  const totalex = parts[0].exercises+parts[1].exercises+parts[2].exercises
   return (
     <div>
       <p>Number of exercises {totalex}</p>
