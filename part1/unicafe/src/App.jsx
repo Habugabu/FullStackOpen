56498ff4cb8daa80240d627570dd391c141c58b0
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-02 15:22:19 +0900

    1.6-1.11 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,85 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  //const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+  const total = () => good + neutral + bad
+  const average = () => {
+    if (total() == 0) return 0
+    else return (good - bad)/(total())
+  }
+  const positive = () => {
+    if (total() == 0) return 0
+    else return (100 * (good)/(total())) + '%'
+  }
 
   return (
-    <>
+    <div>
+      <Header text = 'give feedback'/>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button onClick={handleGoodClick} text = 'good' />
+        <Button onClick={handleNeutralClick} text = 'neutral' />
+        <Button onClick={handleBadClick} text = 'bad' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Header text = 'statistics'/>
+      <Stats stats = {{good: good, neutral: neutral, bad: bad,
+         total: total(), average: average(), positive: positive()}}/>
+    </div>
   )
 }
+
+const Header = ({text}) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Stats = ({stats}) => {
+  if (stats.total == 0){
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  else return (
+    <table>
+      <tbody>
+        <Statistic text = 'good' value = {stats.good}/>
+        <Statistic text = 'neutral' value = {stats.neutral}/>
+        <Statistic text = 'bad' value = {stats.bad}/>
+        <Statistic text = 'total' value = {stats.total}/>
+        <Statistic text = 'average' value = {stats.average}/>
+        <Statistic text = 'positive' value = {stats.positive}/>
+      </tbody>
+    </table>
+
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick = {onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistic = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 
 export default App
